1a0c8649d87e22ed9c188149f4dd326445630960
Yuqing Wu <ywu20@stuy.edu>
2020-11-20 13:19:30 -0500

    equal()

---
+++ RealNumber.java
@@ -0,0 +1,61 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+  //---------ONLY EDIT BELOW THIS LINE------------
+
+  /*
+  *Return true when the values are within 0.001% of eachother.
+  *Special case: if one is exactly zero, the other must be exactly zero.
+  */
+  public boolean equals(RealNumber other){
+    if(Math.abs((other.getValue()-value)/value)<=0.00001 || Math.abs((other.getValue()-value)/other.getValue())<=0.00001){
+      return true;
+    }
+    return false;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the sum of this and the other
+  */
+  public RealNumber add(RealNumber other){
+     //other can be ANY RealNumber, including a RationalNumber
+     //or other subclasses of RealNumber (that aren't written yet)
+     return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the product of this and the other
+  */
+  public RealNumber multiply(RealNumber other){
+        return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this divided by the other
+  */
+  public RealNumber divide(RealNumber other){
+        return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this minus the other
+  */
+  public RealNumber subtract(RealNumber other){
+    return null;
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester{
+  public static void main(String[] args){
+    RealNumber a = new RealNumber(10.453);
+    RealNumber b= new RealNumber(100.2);
+    RealNumber c= new RealNumber(Math.sqrt(2));
+    RealNumber d= new RealNumber(10.4531);
+
+    System.out.println(a.equals(d));
+  }
+}
